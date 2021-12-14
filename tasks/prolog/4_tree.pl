3f6f75358d15f221ed7530abe1592eddcfa362f4
dongmengyu20211213 <96064934+dongmengyu20211213@users.noreply.github.com>
2021-12-14 15:06:15 +0800

    Create 4_tree.pl

--- tasks/prolog/4_tree.pl
+++ tasks/prolog/4_tree.pl
@@ -3,3 +3,24 @@
 % для построения дерева использовать следующие  нотации:
 % empty - пустое дерево 
 % instant(R, L, R) - бинарное дерево с корнем R и двумя поддеревьями L и R соотвественно (левое и правое)
+
+:- consult(‘3_quicksort.pl’)
+
+balanced_tree(L,T):-
+  qsort(L,K),
+  build_tree(K,T).
+  
+  build_tree([],empty).
+  build_tree(L,instant(A,B,C)):-
+                            div(L,X,[A|Y]),
+                            build_tree(X,B),
+                            build_tree(Y,C).
+                            
+      
+      
+      div(L,X,Y):-
+            length(L,S),
+            H is S-S//2,
+            length(Y,H),
+            append(X,Y,L).
+            
