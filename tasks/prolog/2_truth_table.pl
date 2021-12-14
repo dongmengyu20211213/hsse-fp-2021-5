26dfb8b38a10738b143f82c22da3aaaab4cf6b0a
dongmengyu20211213 <96064934+dongmengyu20211213@users.noreply.github.com>
2021-12-14 14:14:12 +0800

    Update 2_truth_table.pl

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -10,3 +10,33 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+and(A,B):-A,B.
+or(A,B):-not(and(not(A),not(B)).
+xor(A,B):-or(and(not(A),B),and(A,not(B)).
+not(A):-\A.
+equ(A,B):- not(xor(A,B) .
+
+estimate(C, true) :- C, !. 
+estimate(_, false). 
+
+boole(true). 
+boole(false). 
+
+truth_table(A,B,C) :- 
+		    boole(A), 
+		    boole(B), 
+		    write(A), 
+		    write(' \t '), 
+		    write(B), 
+		    write(' \t '), 
+		   estimate(C, Result), 
+			write(Result), 
+			fail. 
+			
+?-truth_table(A,B,and(A,B)).
+true     true    true
+true     false   false
+false    true    false
+false    false   false
+false.
